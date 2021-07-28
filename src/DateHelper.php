07caaa1b67e30837c15c616cf3b426cf5f323720
Lucian Filote <lucian.filote@speqta.com>
2021-07-28 20:48:16 +0300

    Initial push

--- src/DateHelper.php
+++ src/DateHelper.php
@@ -8,18 +8,28 @@ namespace LucianOvidiuFilote\DateHelper;
  */
 class DateHelper
 {
+    public static function setTimeStart(\DateTime $date)
+    {
+        return $date->setTime(0, 0, 0);
+    }
+
+    public static function setTimeEnd(\DateTime $date)
+    {
+        return $date->setTime(23, 59, 59);
+    }
+
     public static function getCurrentMonth()
     {
-        return (new \DateTime())->modify('first day of this month');
+        return self::setTimeStart((new \DateTime())->modify('first day of this month'));
     }
 
     public static function getMonthStart(\DateTime $date)
     {
-        return (clone $date)->modify('first day of this month')->setTime(0, 0, 0);
+        return self::setTimeStart((clone $date)->modify('first day of this month'));
     }
 
     public static function getMonthEnd(\DateTime $date)
     {
-        return (clone $date)->modify('last day of this month')->setTime(23, 59, 59);
+        return self::setTimeEnd((clone $date)->modify('last day of this month'));
     }
 }
